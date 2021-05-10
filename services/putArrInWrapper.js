30d82d503e0949fe49eef5430c87607c54d6ead7
Srr233 <Sr233@yandex.ru>
2021-05-10 14:08:23 +0300

    feat: implement putArrInWrapper function

---
+++ services/putArrInWrapper.js
@@ -0,0 +1,19 @@
+function createCard (num) {
+  const divWrapper = document.createElement('div');
+  const div = document.createElement('div');
+  divWrapper.insertAdjacentElement('beforeend', div);
+
+  div.className = 'card';
+  div.textContent = num;
+  div.dataset.index = num;
+
+  return divWrapper;
+}
+export default function putArrInWrapper (wrapper, arr) {
+  const copyArr = arr.slice();
+
+  const arrInLine = [...copyArr[0], ...copyArr[1], ...copyArr[2], ...copyArr[3]];
+  arrInLine.forEach(item => {
+    wrapper.insertAdjacentElement('beforeend', createCard(item));
+  });
+}
