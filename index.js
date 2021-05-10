177cad7947ef3375d9ba2c4c584f37b515335b8a
Srr233 <Sr233@yandex.ru>
2021-05-10 22:29:31 +0300

    refactor: change postition

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
 import init from './services/init.js';
-import mainClassGame from './services/constants/mainClass.js'
-import solving from './services/functionsForUsing/solving.js';
-import transfer from './services/functionsForUsing/transfer.js';
-import autoSolving from './services/functionsForUsing/auto-solving.js';
-
-window.autoSolving = autoSolving;
-window.solving = solving;
-window.transfer = transfer;
 
 const wrapper = document.querySelector(mainClassGame);
 
--- services/init.js
+++ services/init.js
@@ -1,9 +1,17 @@
 import createArr from './createArr.js';
 import { sortGem } from './sortOfGameArr.js';
 import putArrInWrapper from './putArrInWrapper.js';
+import solving from './functionsForUsing/solving.js';
+import transfer from './functionsForUsing/transfer.js';
+import autoSolving from './functionsForUsing/auto-solving.js';
+
 
 export default function initMapOfGame(wrapper) {
-  const arr = createArr();
-  const sortedArr = sortGem(arr);
+  const sortedArr = sortGem(createArr());
+
+  window.autoSolving = autoSolving(sortedArr);
+  window.solving = solving(sortedArr);
+  window.transfer = transfer(sortedArr);
+  
   putArrInWrapper(wrapper, sortedArr);
 }
