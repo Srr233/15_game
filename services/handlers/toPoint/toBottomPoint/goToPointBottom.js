09c0d9a26d9c4d760ccb390fd9b83e0a6480d0b0
Srr233 <Sr233@yandex.ru>
2021-05-24 20:21:25 +0300

    refactor: refactor goToPointBottom func

--- services/handlers/toPoint/toBottomPoint/goToPointBottom.js
+++ services/handlers/toPoint/toBottomPoint/goToPointBottom.js
@@ -1,4 +1,4 @@
-import { moreWays } from '../moreWays.js';
+import { getMoreCorrectWays } from '../getMoreCorrectWays.js';
 
 function isSpaceLeft(chosenPos, way) {
   if (chosenPos[1] > way.spacePos[1]) {
@@ -8,57 +8,32 @@ function isSpaceLeft(chosenPos, way) {
 }
 
 function spaceUp(chosenPos, way) {
-  /* do we have spaces for?
-  [3, 8, 7]
-  [9, 12, 16]
-  */
-  let isEnoughSpaces;
-
+  const options = {
+    direction: 'left bottom', whereIsSpace: 'up',
+    isEnoughSpaces: chosenPos[1] - 2 >= 0, chosenPos
+  };
   if (isSpaceLeft(chosenPos, way) === 'right') {
-    isEnoughSpaces = () => chosenPos[1] + 2 <= 3;
-    if (isEnoughSpaces()) {
-      return moreWays(chosenPos, 'right bottom', 'up');
-    }
-    return moreWays(chosenPos, 'right bottom', 'up', false);
+    options.direction = 'right bottom';
+    options.isEnoughSpaces = chosenPos[1] + 2 <= 3;
+    return getMoreCorrectWays(options);
   }
-
-  isEnoughSpaces = () => chosenPos[1] - 2 >= 0;
-  if (isEnoughSpaces()) {
-    return moreWays(chosenPos, 'left bottom', 'up');
-  }
-  return moreWays(chosenPos, 'left bottom', 'up', false);
+  return getMoreCorrectWays(options);
 }
 function spaceDown(chosenPos, way) {
-  /* do we have spaces for?
-  [3, 8, 7]
-  [9, 16, 12]
-  */
-  let isEnoughSpaces;
-
+  const options = {
+    direction: 'left bottom', whereIsSpace: 'down',
+    isEnoughSpaces: chosenPos[1] - 2 >= 0, chosenPos
+  };
   if (isSpaceLeft(chosenPos, way) === 'right') {
-    isEnoughSpaces = () => chosenPos[1] + 2 <= 3;
-    if (isEnoughSpaces()) {
-      return moreWays(chosenPos, 'right bottom', 'down');
-    }
-    return moreWays(chosenPos, 'right bottom', 'down', false);
+    options.direction = 'right bottom';
+    options.isEnoughSpaces = chosenPos[1] + 2 <= 3;
+    return getMoreCorrectWays(options);
   }
-
-  isEnoughSpaces = () => chosenPos[1] - 2 >= 0;
-  if (isEnoughSpaces()) {
-    return moreWays(chosenPos, 'left bottom', 'down');
-  }
-  return moreWays(chosenPos, 'left bottom', 'down', false);
+  return getMoreCorrectWays(options);
 }
 
 function goToPointBottom(chosenPos, way) {
-  let ways;
-  /* space up or down 
-  [1, 16, 2] [1, 4, 2]
-  [3, 4, 7] [3, 16, 7]
-  */
-  if (chosenPos[0] === way.spacePos[0]) {
-    return spaceUp(chosenPos, way);
-  }
+  if (chosenPos[0] === way.spacePos[0]) return spaceUp(chosenPos, way);
   return spaceDown(chosenPos, way);
 }
 
