3b02bc1ee033924bade09bd89fd70375b522a7ee
Srr233 <Sr233@yandex.ru>
2021-05-24 18:19:38 +0300

    refactor: change the name of variable

--- services/functionsForUsing/transfer.js
+++ services/functionsForUsing/transfer.js
@@ -8,10 +8,10 @@ export default function transfer (arr) {
   const message = () => console.log('You can go only around the space');
 
   return (from, to) => {
-    const way = getPositionOfSpaceAndGoal(from, to, arr);
+    const positions = getPositionOfSpaceAndGoal(from, to, arr);
     const currentPos = getCurrentPos(from, arr);
-    if (isCorrectWay(way, currentPos)) {
-      const listOfWays = getListOfWays(way, currentPos, arr);
+    if (isCorrectWay(positions, currentPos)) {
+      const listOfWays = getListOfWays(positions, currentPos, arr);
       if (listOfWays) {
         walk(listOfWays, arr);
         if (isDone(arr)) console.log('The game is solved');
