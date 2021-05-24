9c78ba6882b8b3f445d5b4f8b6a6ad36f3a08d06
Srr233 <Sr233@yandex.ru>
2021-05-24 19:32:51 +0300

    refactor: refactor getListOfWays

--- services/handlers/getListOfWays.js
+++ services/handlers/getListOfWays.js
@@ -2,40 +2,25 @@ import go from './ways.js';
 
 
 function getListForArr(way, chosenPos) {
-  const isToSpace = (way) => {
-    return way.spacePos.every((n) => way.toPos.includes(n));
-  }
-  if (isToSpace(way)) {
-    return [{n: chosenPos, to: way.spacePos}];
-  }
+  const isToSpace = (way) => way.spacePos.every((n) => way.toPos.includes(n));
+  if (isToSpace(way)) return [{n: chosenPos, to: way.spacePos}];
 }
 
 
 function getListForArrAngleToSpace(way, chosenPos) {
   const SIZE = 3;
-  switch (chosenPos[0]) {
-    case SIZE:
-      return go.rotateFromDown(chosenPos, way.spacePos);
-    case SIZE - SIZE:
-      return go.rotateFromUp(chosenPos, way.spacePos);
-    default:
-      return go.rotateFromMiddle(chosenPos, way.spacePos);
-  }
+  const line = chosenPos[0];
+  if (line === SIZE) return go.rotateFromDown(chosenPos, way.spacePos);
+  if (line === 0) return go.rotateFromUp(chosenPos, way.spacePos);
+  return go.rotateFromMiddle(chosenPos, way.spacePos);
 }
 
 
 function getListForArrToPoint(way, chosenPos) {
-  if (chosenPos[0] > way.toPos[0]) {
-    return go.goToPointUp(chosenPos, way);
-  } else if (chosenPos[0] < way.toPos[0]){
-    return go.goToPointBottom(chosenPos, way);
-  } else {
-    if (chosenPos[1] > way.toPos[1]) {
-      return go.goToPointLeft(chosenPos, way);
-    } else {
-      return go.goToPointRight(chosenPos, way);
-    }
-  }
+  if (chosenPos[0] > way.toPos[0]) return go.goToPointUp(chosenPos, way);
+  if (chosenPos[0] < way.toPos[0]) return go.goToPointBottom(chosenPos, way);
+  if (chosenPos[1] > way.toPos[1]) return go.goToPointLeft(chosenPos, way);
+  return go.goToPointRight(chosenPos, way);
 }
 
 
