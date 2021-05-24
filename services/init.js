3e2614ca3b74507555e9213d6ca6bcea34890f8a
Srr233 <Sr233@yandex.ru>
2021-05-24 17:57:44 +0300

    refactor: initMapOfGame

--- services/init.js
+++ services/init.js
@@ -5,11 +5,13 @@ import solving from './functionsForUsing/solving.js';
 import transfer from './functionsForUsing/transfer.js';
 import autoSolving from './functionsForUsing/auto-solving.js';
 
-
-export default function initMapOfGame() {
-  let sortedArr = sortGem(createArr());
+function tieFunctionsToWindow (sortedArr) {
   window.autoSolving = autoSolving(sortedArr);
   window.solving = solving(sortedArr);
   window.transfer = transfer(sortedArr);
+}
+export default function initMapOfGame() {
+  let sortedArr = sortGem(createArr());
+  tieFunctionsToWindow(sortedArr);
   putArrInWrapper(sortedArr);
 }
