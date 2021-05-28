06028d49c6bbee30e8ffb8ed281b5e5ebb1ccd48
Srr233 <Sr233@yandex.ru>
2021-05-28 16:19:03 +0300

    refactro: refactor checkSides functions

--- services/handlers/checkSides.js
+++ services/handlers/checkSides.js
@@ -1,115 +1,40 @@
 import EMPTY_POS from '../constants/emptyCardNum.js';
+import { getCurrentPos } from './checkMove.js';
 
+function getDifferenceIsMore1(n1, n2) {
+  function toPositiveNumber(n) {
+    if (n < 0) return Math.abs(n);
+    return n;
+  }
+  return toPositiveNumber(n1 - n2) > 1
+}
 function checkSides(chosenPos, direction, arr) {
-  let isContain;
-  switch (chosenPos[1]) {
-    case 0:
-      isContain = arr[chosenPos[0]][1] === direction;
-      if (isContain) {
-        return [chosenPos[0], 1];
-      }
-      return [];
-    case 3:
-      isContain = arr[chosenPos[0]][2] === direction;
-      if (isContain) {
-        return [chosenPos[0], 2];
-      }
-      return [];
-    default:
-      const isLeftContain = arr[chosenPos[0]][chosenPos[1] - 1] === direction;
-      const isRightContain = arr[chosenPos[0]][chosenPos[1] + 1] === direction;
-
-      if (isLeftContain) {
-        return [chosenPos[0], chosenPos[1] - 1];
-      } else if (isRightContain) {
-        return [chosenPos[0], chosenPos[1] + 1];
-      }
-      return [];
-  }
+  const [directionArr, directionIndex] = getCurrentPos(direction, arr);
+  const [chosenArr, chosenIndex] = chosenPos;
+  const differenceIsMore1 = getDifferenceIsMore1(chosenIndex, directionIndex);
+  if (differenceIsMore1) return [];
+  if (chosenArr !== directionArr) return [];
+  if (chosenIndex > directionIndex) return [chosenArr, chosenIndex - 1];
+  return [chosenArr, chosenIndex + 1];
 }
-
-
-
-
 function checkUpSides(chosenPos, direction, arr) {
-  switch (chosenPos[0]) {
-    case 0:
-      if (arr[1][chosenPos[1]] === direction) {
-        return [1, chosenPos[1]];
-      }
-      return [];
-    case 3:
-      if (arr[2][chosenPos[1]] === direction) {
-        return [2, chosenPos[1]];
-      }
-      return [];
-    default:
-      if (arr[chosenPos[0] + 1][chosenPos[1]] === direction) {
-        return [chosenPos[0] + 1, chosenPos[1]];
-      } else if (arr[chosenPos[0] - 1][chosenPos[1]] === direction) {
-        return [chosenPos[0] - 1, chosenPos[1]];
-      }
-      return [];
-  }
+  const [directionArr, directionIndex] = getCurrentPos(direction, arr);
+  const [chosenArr, chosenIndex] = chosenPos;
+  const differenceIsMore1 = getDifferenceIsMore1(chosenArr, directionArr);
+  if (differenceIsMore1) return [];
+  if (chosenIndex !== directionIndex) return [];
+  if (chosenArr > directionArr) return [chosenArr - 1, chosenIndex];
+  return [chosenArr + 1, chosenIndex];
 }
-
 function checkAngles (chosenPos, direction, arr) {
-
-  const checkAngle = (chosenPos, direction, arr) => {
-    const correctDirection = (n) => {
-      return arr.indexOf(direction) === chosenPos + n ? chosenPos + n : false;
-    }
-    switch (chosenPos) {
-      case 0:
-        return correctDirection(1);
-      case 3:
-        return correctDirection(-1);
-      default:
-        const right = correctDirection(1);
-        const left = correctDirection(-1);
-        return (right !== false ? right : false ) || (left !== false ? left : false);
-    }
-  };
-
-  let resultOfAngle = false;
-  let resultOfAngleUp = false;
-  let resultOfAngleDown = false;
-  switch (chosenPos[0]) {
-    case 0:
-      resultOfAngle = checkAngle(chosenPos[1], direction, arr[1]);
-      if (resultOfAngle !== false) {
-        return [1, resultOfAngle];
-      }
-      return [];
-    case 3:
-      resultOfAngle = checkAngle(chosenPos[1], direction, arr[2]);
-      if (resultOfAngle !== false) {
-        return [2, resultOfAngle];
-      }
-      return [];
-    case 1:
-        resultOfAngleUp = checkAngle(chosenPos[1], direction, arr[0]);
-        resultOfAngleDown = checkAngle(chosenPos[1], direction, arr[2]);
-        if (resultOfAngleUp !== false) {
-          return [0, resultOfAngleUp];
-        } else if (resultOfAngleDown !== false) {
-          return [2, resultOfAngleDown];
-        }
-        return [];
-    case 2:
-      resultOfAngleUp = checkAngle(chosenPos[1], direction, arr[1]);
-      resultOfAngleDown = checkAngle(chosenPos[1], direction, arr[3]);
-      if (resultOfAngleUp !== false) {
-        return [1, resultOfAngleUp];
-      } else if (resultOfAngleDown !== false) {
-        return [3, resultOfAngleDown];
-      }
-      return [];
-    default:
-      throw new Error('Current position didn\'t find');
-  }
+  const [directionArr, directionIndex] = getCurrentPos(direction, arr);
+  const [chosenArr, chosenIndex] = chosenPos;
+  const differenceArrIsMore1 = getDifferenceIsMore1(chosenIndex, directionIndex);
+  const differenceIndexIsMore1 = getDifferenceIsMore1(chosenArr, directionArr);
+  if (differenceArrIsMore1 || differenceIndexIsMore1) return [];
+  if (chosenArr > directionArr) return [chosenArr - 1, directionIndex];
+  return [chosenArr + 1, directionIndex];
 }
-
 function searchCardAroundGivenPos (chosenPos, direction, arr) {
   const sides = checkSides(chosenPos, direction, arr);
   const upSides = checkUpSides(chosenPos, direction, arr);
@@ -120,6 +45,9 @@ function searchCardAroundGivenPos (chosenPos, direction, arr) {
       : angles.length > 0 ? angles
         : [];
 }
+
+
+
 function checkTransferToCard (chosenPos, direction, arr) {
   let foundDirection;
   let foundSpace;
