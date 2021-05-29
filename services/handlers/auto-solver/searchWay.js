b1459555ef3ac1a093c6b2484597298b6bd5c44c
Srr233 <Sr233@yandex.ru>
2021-05-29 18:16:11 +0300

    refactor: refactor searchWay func

--- services/handlers/auto-solver/searchWay.js
+++ services/handlers/auto-solver/searchWay.js
@@ -9,20 +9,17 @@ const ways = (point) => ({
     if (point.nArr - 1 >= 0) {
       return { nArr: point.nArr - 1, nIndex: point.nIndex };
     }
-  })()
-  ,
+  })(),
   down: (() => {
     if (point.nArr + 1 < 4) {
       return { nArr: point.nArr + 1, nIndex: point.nIndex };
     }
-  })()
-  ,
+  })(),
   left: (() => {
     if (point.nIndex - 1 >= 0) {
       return { nArr: point.nArr, nIndex: point.nIndex - 1 };
     }
-  })()
-  ,
+  })(),
   right: (() => {
     if (point.nIndex + 1 < 4) {
       return { nArr: point.nArr, nIndex: point.nIndex + 1 };
@@ -39,51 +36,35 @@ function searchWay(arr, start, goal, block) {
     const nIndex = arr[nArr].indexOf(n);
     return { nArr, nIndex };
   };
-  const noHasN = (n) => explored.findIndex(i => {
-    return i.nArr === n.nArr && i.nIndex === n.nIndex
-  }) < 0;
   const s = searchNumber(arr, start);
   const g = searchNumber(arr, goal);
-  let nodes = [];
-
+  
   research.push(s);
   if (block) {
     for (const blocked of block) {
       explored.push(blocked);
     }
   }
+  let nodes = [];
+  const noHasN = (n) => explored.findIndex(i => {
+    return i.nArr === n.nArr && i.nIndex === n.nIndex
+  }) < 0;
   while (research.length) {
     const pop = research.shift();
     explored.push(pop);
 
     let node;
-    if (pop === s) {
-      node = new MyNode(pop, null);
-    } else {
-      node = nodes.find(n => n.n.nArr === pop.nArr && n.n.nIndex === pop.nIndex);
-    }
+    node = nodes.find(n => n.n.nArr === pop.nArr && n.n.nIndex === pop.nIndex);
+    if (pop === s) node = new MyNode(pop, null);
+    
+    if (g.nArr === pop.nArr && g.nIndex === pop.nIndex) return node;
 
-    const way = ways(pop);
-    if (g.nArr === pop.nArr && g.nIndex === pop.nIndex) {
-      research = [];
-      return node;
-    } else {
-      if (way.up && noHasN(way.up)) {
-        nodes.push(new MyNode(way.up, node));
-        research.push(way.up);
-      }
-      if (way.down && noHasN(way.down)) {
-        nodes.push(new MyNode(way.down, node));
-        research.push(way.down);
-      }
-      if (way.left && noHasN(way.left)) {
-        nodes.push(new MyNode(way.left, node));
-        research.push(way.left);
-      }
-      if (way.right && noHasN(way.right)) {
-        nodes.push(new MyNode(way.right, node));
-        research.push(way.right);
-      }
+    const way = Object.values(ways(pop));
+    for (const direction of way) {
+      if (!direction) continue;
+      if (!noHasN(direction)) continue;
+      nodes.push(new MyNode(direction, node));
+      research.push(direction)
     }
   }
 }
